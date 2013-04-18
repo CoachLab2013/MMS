cff05a526dcaf6ee40a0c3917323ac79b92f52a1
kalel314 <200935415@student.uj.ac.za>
2013-04-18 08:22:25 +0200

    Added body,bodyAddress and bodyAtScene classes

---
+++ Team1_Db/TestApp/src/testapp/Body.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author Team 1 CoachLab 2013 Mortuary Manage Managemet System (Chester)
+ */
+public class Body 
+{
+    protected String deathRegisterNumber;
+    protected String gender;
+    protected String race;
+    protected String assignedTo;
+    protected String nameOfDeceased;
+    protected String surnameOfDeceased;
+    protected String placeOfBirth;
+    protected String dateOfBirth;
+    protected int ageOnDateFound;
+    private String martitalStatus;
+    private String occupation;
+    private String citizen;
+    private String maidenName;
+    private String dataTimeIdentified;
+    private BodyAddress bodyAddress;
+    public Body()
+    {
+        
+    }
+    
+}
---
+++ Team1_Db/TestApp/src/testapp/BodyAtMortuary.java
@@ -0,0 +1,14 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author Chester
+ */
+public class BodyAtMortuary extends Body
+{
+    
+}
