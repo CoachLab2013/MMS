69ddce17aa1a88dc822f6efccaa0ad1739c6e877
mmsCoachLab <200935415@students.uj.ac.za>
2013-04-23 11:00:29 +0200

    added Special Circumstance reference table <Bandile>

---
+++ Team1_Db/TestApp/src/testapp/BodyDb.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class BodyDb extends DatabaseConnector{
+    Body body;
+    //CONSTRUCTORS
+    public BodyDb(DbDetail dbDetail){
+        super(dbDetail);
+    }
+    
+    //GET METHODS
+    
+    //SET METHODS
+    
+    //DATABASE METHODS
+    @Override
+    public String add(){
+        return null;
+        
+    }
+    
+    @Override
+    public String read(){
+        return null;
+        
+    }
+    
+    @Override
+    public String edit(){
+        return null;
+        
+    }
+    
+    @Override
+    public String delete(){
+        return null;
+        
+    }
+}
