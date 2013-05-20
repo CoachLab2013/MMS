e4291a1e766f1d328b55f04b7e05d9265a0b2cea
BandileN <bandilendlela@gmail.com>
2013-05-20 11:24:48 +0200

    Changed database connector password

--- Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
+++ Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
@@ -17,7 +17,7 @@ public class Tools {
      * constructor for an instance of Tools
      */
     public Tools(){
-        dbdetail = new DbDetail("localhost","/mydb","root","password"); 
+        dbdetail = new DbDetail("localhost","/mydb","root","password123"); 
     }
     //end constructor
     
