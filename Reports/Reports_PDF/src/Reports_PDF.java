449ba105b70065dd3058b4c6c1af8afe86f788f0
Mabilu <snowy.mabilu@gmail.com>
2013-05-20 08:38:52 +0200

    Updating changes

--- Reports/Reports_PDF/src/Reports_PDF.java
+++ Reports/Reports_PDF/src/Reports_PDF.java
@@ -29,11 +29,11 @@ public class Reports_PDF {
             Connection connection = new DatabaseConnector(DbDetail.staticDBDetail).init();
                 new Report_AuditTrail("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Audit Trail Statistics", connection).createReport();
                 new Report_BodiesByOrganisation("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Bodies By Organisation", connection).createReport();
-                new Report_FacilityStorage("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Facility Storage", connection).createReport();
+                //new Report_FacilityStorage("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Facility Storage", connection).createReport();
                 new Report_MannerOfDeath("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Manner of Death", connection).createReport();
                 new Report_MannerOfDeathStatistics("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Manner of Death Statistics", connection).createReport();
                 new Report_UnidentifiedBodiesStatistics("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Unidentified Bodies Statistics", connection).createReport();
-                new Report_TurnAroundOnResults("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Turn Around on Results", connection).createReport();  
+               // new Report_TurnAroundOnResults("./" + new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()) + " Report - Turn Around on Results", connection).createReport();  
             connection.close();
         } catch (SQLException ex) {
             Logger.getLogger(Reports_PDF.class.getName()).log(Level.SEVERE, null, ex);
