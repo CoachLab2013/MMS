494558fe07673ecf52fc5605cdec9c237b5b8862
chester314 <200935415@student.uj.ac.za>
2013-05-21 09:36:23 +0200

    Updated database dump called "MMS_Dump20130521" and some changes to the kin jsp page

---
+++ Integrated Project/Gauteng MMS/src/java/servlets/SaveKinDetailsServlet.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+import database.DbDetail;
+import database.Kin;
+import database.KinDb;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Chester
+ */
+@WebServlet(name = "SaveKinDetailsServlet", urlPatterns = {"/SaveKinDetailsServlet"})
+public class SaveKinDetailsServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Kin kin = new Kin();
+        kin.setName(request.getParameter("KinName"));
+        kin.setSurname(request.getParameter("KinSurname"));
+        String kinIdType = request.getParameter("identificationtype");
+        if(kinIdType.contains("ID"))
+        {
+            kin.setID(request.getParameter("KinIDNumber"));
+        }
+        else if(kinIdType.contains("Passport"))
+        {
+            kin.setPassport(request.getParameter("KinIDNumber"));
+        }
+        kin.setRelationWithDeceased(request.getParameter("KinRelationship"));
+        kin.setContactNumber(request.getParameter("KinContact"));
+        kin.setAddress(request.getParameter("KinRes"));
+        kin.setWorkAddress(request.getParameter("KinWork"));
+        kin.setBody_idDeathRegisterNumber("099888592");
+        Tools t = new Tools();
+        DbDetail dbdetail = t.getDbdetail();
+        KinDb kinDb = new KinDb(kin, dbdetail);
+        kinDb.init();
+        kinDb.add();
+        HttpSession sess = request.getSession();
+        sess.setAttribute("kinDetail", "Kin details added successfully");
+        response.sendRedirect("Home.jsp");
+        out.close();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
