d997851ca0e9ab73f0ee48214c77584a5c49a22e
chester314 <200935415@student.uj.ac.za>
2013-05-21 11:09:29 +0200

    added new class

--- Integrated Project/Gauteng MMS/web/js/KinDetailScript.js
+++ Integrated Project/Gauteng MMS/web/js/KinDetailScript.js
@@ -84,14 +84,12 @@ $(document).ready(function(){
     
     if($("#go_to_deceasedDetails").val()=="true")
     {
-         //set focus on Open Incidents tab 
-            $("#OpenIncidentsTab").removeClass("active");
-            $("#EditIncidentTab").addClass("active");
-            
-            $("#OpenIncidents").removeClass("tab-pane active");
-            $("#OpenIncidents").addClass("tab-pane");
-            $("#EditIncident").removeClass("tab-pane");
-            $("#EditIncident").addClass("tab-pane active");
+         //$("#IncidentDetailsTab").removeClass("active");
+            $("#IncidentDetails").removeClass("tab-pane active");
+            $("#CallDetails").removeClass("tab-pane");
+            $("#IncidentDetails").addClass("tab-pane");
+            $("#CallDetails").addClass("tab-pane active");
+            $("#CallDetailsTab").addClass("active");
     }
 }); 
 //end $(document).ready(function())
