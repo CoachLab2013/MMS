d7dd315e45d28a11de1dbfd2cd95e658fedcd8cb
Mabilu <snowy.mabilu@gmail.com>
2013-05-27 10:01:45 +0200

    Corrected the reports information in git

---
+++ Reports/Reports_Excel/src/jcse/coachlab2013/mms/reports/Template_Report.java
@@ -0,0 +1,26 @@
+package jcse.coachlab2013.mms.reports;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+/**
+ * @author      Mubien Nackoda <coachlab@jcse.org.za>
+ * @since       2012-05-20          (the version of the package this class was first added to)
+ */
+public abstract class Template_Report {
+    
+    protected String destination;
+    protected Connection connection;
+    
+    protected ResultSet reportData;
+    
+    public Template_Report(String destination, Connection connection)
+    {
+        this.connection = connection;
+        this.destination = destination;
+    }
+    
+    protected abstract void formatData();
+    
+    protected abstract void createReport();
+}
