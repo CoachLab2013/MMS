45db971b59d21a70060b0f9c7af29ba743acadd7
BandileN <bandilendlela@gmail.com>
2013-05-28 09:25:59 +0200

    build files and tools password <Bandile>

--- Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
+++ Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
@@ -19,7 +19,7 @@ public class Tools {
      */
     public Tools(){
  
-        dbdetail = new DbDetail("localhost","/mydb","root","password");
+        dbdetail = new DbDetail("localhost","/mydb","root","password123");
 
     }
     //end constructor
