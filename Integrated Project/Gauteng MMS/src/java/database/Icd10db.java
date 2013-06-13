ff6d94303439c4df5f2038edd1cf4bdcc1f08015
Asheen <sheensin@gmail.com>
2013-06-13 11:35:55 +0200

    Added the ICD10DB file and a dump of the database

--- Integrated Project/Gauteng MMS/src/java/database/Icd10db.java
+++ Integrated Project/Gauteng MMS/src/java/database/Icd10db.java
@@ -4,13 +4,19 @@
  */
 package database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Chester
  */
-public class Icd10db extends DatabaseConnector
+public class ICD10DB extends DatabaseConnector
 {
-    public Icd10db(DbDetail dbDetail)
+    public ICD10DB(DbDetail dbDetail)
     {
         super(dbDetail);
         
@@ -37,5 +43,74 @@ public class Icd10db extends DatabaseConnector
     public String delete() 
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public ArrayList<String> filterICD10Code2 (String ICDLevel1) {
+        ArrayList<String> list = new ArrayList<String>();
+        
+        try {
+            this.init();
+            statement.executeQuery("SELECT\n" +
+                "`icdlevel2`.`diag1_Id`,\n" +
+                "`icdlevel2`.`description`\n" +
+                "FROM `mydb`.`icdlevel2`\n" +
+                "WHERE `icdlevel2`.`ICDLevel1_chapter` = '" + ICDLevel1 + "';");
+            
+            ResultSet resultset = statement.getResultSet();
+            while (resultset.next()) {
+                list.add(resultset.getString("diag1_Id") + " " + resultset.getString("description"));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ICD10DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
+    }
+    
+    public ArrayList<String> filterICD10Code3 (String ICDLevel2) {
+        ArrayList<String> list = new ArrayList<String>();
+        
+        try {
+            this.init();
+            statement.executeQuery("SELECT\n" +
+                "`icdlevel3`.`diag2_Id`,\n" +
+                "`icdlevel3`.`description`\n" +
+                "FROM `mydb`.`icdlevel3`\n" +
+                "WHERE `icdlevel3`.`diag1_Id` = '" + ICDLevel2 + "';");
+            
+            ResultSet resultset = statement.getResultSet();
+            while (resultset.next()) {
+                list.add(resultset.getString("diag2_Id") + " " + resultset.getString("description"));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ICD10DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
+    }
+    
+    public ArrayList<String> filterICD10Code4 (String ICDLevel3) {
+        ArrayList<String> list = new ArrayList<String>();
+        
+        try {
+            this.init();
+            statement.executeQuery("SELECT\n" +
+            "`icdlevel4`.`diag3_Id`,\n" +
+            "`icdlevel4`.`description`\n" +
+            "FROM `mydb`.`icdlevel4`\n" +
+            "WHERE `icdlevel4`.`ICDLevel3_dia2Id` = '" + ICDLevel3 + "';");
+            
+            ResultSet resultset = statement.getResultSet();
+            while (resultset.next()) {
+                list.add(resultset.getString("diag3_Id") + " " + resultset.getString("description"));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ICD10DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
     }
 }
