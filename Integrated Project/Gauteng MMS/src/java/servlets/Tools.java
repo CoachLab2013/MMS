01995ce048aac0ba0cfb501fd9f2f314f878b8cc
BandileN <bandilendlela@gmail.com>
2013-06-20 12:27:06 +0200

    build files <Bandile>

--- Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
+++ Integrated Project/Gauteng MMS/src/java/servlets/Tools.java
@@ -23,7 +23,7 @@ public class Tools {
      */
     public Tools()
     {
-        dbdetail = new DbDetail("localhost", "/mydb", "root", "hello");
+        dbdetail = new DbDetail("localhost", "/mydb", "root", "password123");
     }
     //end constructor
 
